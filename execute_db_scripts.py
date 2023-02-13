ad132ca530b739e20a2cb351f7a0a74e52308597
balendrapratap1212 <119517874+balendrapratap1212@users.noreply.github.com>
2023-02-13 14:01:41 +0530

    Update execute_db_scripts.py

--- execute_db_scripts.py
+++ execute_db_scripts.py
@@ -1,6 +1,7 @@
 import os
-# import psycopg2
+//import psycopg2
 import pyodbc
+import pandas
 import configparser
 import json
 from datetime import datetime
@@ -19,24 +20,18 @@ def execute_query(path, service, version):
     failed_queries=''
     conn = pyodbc.connect( host=hostname, user=username, password=password, dbname=database )
     cur = conn.cursor()
-    print(cur)
     file = open(path,'r')
     data = file.read()
     if data:
         query_data = data.replace("\n","")
         queries = query_data.split(";")
-        print(query)
         queries.pop()
         for query in queries:
             try:
-                cur.execute('select * from dbo.Course')
-                records = cur.fetchall()
-                print(records)
-                print('query executed')
+                cur.execute(query)
                 conn.commit()
             except Exception as e:
                 failed_queries = failed_queries + str(e)+"\n"
-                print(failed_queries + str(e))
                 conn.rollback()
                 continue
         conn.close()
@@ -51,7 +46,7 @@ def add_logs(new_logs, new_failed_queries):
         with open(ROOT+'/Failed_Queries.txt', 'r') as original: old_failed_queries = original.read()
         with open(ROOT+'/Failed_Queries.txt', 'w') as modified: modified.write(new_failed_queries+"\n-----\n" + old_failed_queries)
 
-def run_base_script(services) :
+def run_base_script() :
     try:
         logs=''
         error_flag = 0
@@ -63,21 +58,18 @@ def run_base_script(services) :
                 data, fail_queries_ddl=execute_query(ddl_path,service, version)
                 data, fail_queries_dml=execute_query(dml_path,service, version)
                 failed_queries = fail_queries_ddl+fail_queries_dml
-                print(failed_queries)
 
                 logs = logs + "Successfully Executed base script for " + service + " version " + version+ " at " + str(datetime.now())+"\n"
                 with open(sprint_delta_path, 'w') as original: original.truncate(0)
-            except Exception as e:
+            except:
                 logs = logs + "Error while Executed base script for " + service + " version " + version+ " at " + str(datetime.now())+"\n"
-                print(f'LIne NO: 65: {e}')
                 error_flag = 1
                 continue
         # CONFIG.set("EXECUTE_SCRIPTS","services","")
         with open(CONFIG_FILE, 'w') as original: CONFIG.write(original)
-        # add_logs(logs, failed_queries)
+        add_logs(logs, failed_queries)
         response = "Successfully executed base scripts" if error_flag==1 else "Partially executed base scripts"
-    except Exception as e:
-        print(f'Line no 73: {e}')
+    except:
         response = "Error while executing base scripts"
     return response
 
@@ -95,8 +87,7 @@ def run_delta_script() :
                 with open(service_delta_path, 'w') as modified: modified.write("---Service Version "+version+"\n"+sprint_delta_data+"\n-----\n" + old_data)
                 logs = logs + "Successfully Executed delta for " + service + " version " + version+ " at " + str(datetime.now())+"\n"
                 with open(sprint_delta_path, 'w') as original: original.truncate(0)
-            except Exception as e:
-                print(f'Line no:92: {e}')
+            except:
                 logs = logs + "Error while Executed delta for " + service + " version " + version+ " at " + str(datetime.now())+"\n"
                 error_flag = 1
                 continue
@@ -104,13 +95,12 @@ def run_delta_script() :
         with open(CONFIG_FILE, 'w') as original: CONFIG.write(original)
         add_logs(logs, failed_queries)
         response = "Successfully executed delta scripts" if error_flag==1 else "Partially executed delta scripts"
-    except Exception as e:
-        print(f'Line no: 101: {e}')
+    except:
         response = "Error while executing delta scripts"
     return response
 
 
 services = CONFIG.get("EXECUTE_SCRIPTS","services").split(',')
 script_type = CONFIG.get("EXECUTE_SCRIPTS","type")
-run_script = run_delta_script() if script_type =='delta' else run_base_script(services)
+run_script = run_delta_script() if script_type =='delta' else run_base_script()
 print(run_script)
